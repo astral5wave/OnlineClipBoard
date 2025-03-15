f46097b4091944572c073a6d5f4a5b051cce9286
astral5wave <143374637+astral5wave@users.noreply.github.com>
2025-03-16 02:26:50 +0530

    Update main.go
    commented env file setup for deployment

--- backend/main.go
+++ backend/main.go
@@ -62,10 +62,10 @@ func generateUniqueKey() (string, error) {
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	port := os.Getenv("PORT")
 	if port == "" {
